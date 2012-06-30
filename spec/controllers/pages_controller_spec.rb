0ab636c9bc5e781d3a435534ed0d248f1ae366aa
Kannan Maikeffi <maikeffi@gmail.com>
2012-06-30 17:11:38 +0530

    Modified all pages to use a dynamic template

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -10,8 +10,12 @@ describe PagesController do
     end
     it "should have right title" do
       get 'home'
-      response.should have_selector("title", :content =>"Ruby on rails tutorial : sample app |Home")
+      response.should have_selector("title", :content =>"Ruby on rails tutorial : sample app | Home")
     end
+    it "should not have blank body" do
+    	get 'home'
+    	response.body.should_not =~ /<body>\s*<\/body>/
+	end
   end
 
   describe "GET 'contact'" do
@@ -21,8 +25,12 @@ describe PagesController do
     end
     it "should have right title" do
       get 'contact'
-      response.should have_selector("title", :content =>"Ruby on rails tutorial : sample app |Contact")
+      response.should have_selector("title", :content =>"Ruby on rails tutorial : sample app | Contact")
     end
+    it "should not have blank body" do
+    	get 'contact'
+    	response.body.should_not =~ /<body>\s*<\/body>/
+	end
   end
 	describe "GET 'about'" do
     it "should be successful" do
@@ -31,7 +39,11 @@ describe PagesController do
     end
     it "should have right title" do
       get 'about'
-      response.should have_selector("title", :content =>"Ruby on rails tutorial : sample app |About")
+      response.should have_selector("title", :content =>"Ruby on rails tutorial : sample app | About")
     end
+    it "should not have blank body" do
+    	get 'about'
+    	response.body.should_not =~ /<body>\s*<\/body>/
+	end
   end
 end
